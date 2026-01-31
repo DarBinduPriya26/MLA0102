398e6b08f58140a0a4f32cf2f3053d3498bc80fb
DarBinduPriya26 <192425162.simats@saveetha.com>
2026-01-31 09:33:51 +0530

    Add files via upload

---
+++ playtennis.py
@@ -0,0 +1,13 @@
+def print_decision_tree():
+    print("a1")
+    print(" " * 22 + "/         \\")
+    print(" " * 17 + "False             True")
+    print(" " * 18 + "Yes               a3")
+    print(" " * 11 + "(3,4,5,9,10)          /        \\")
+    print(" " * 30 + "High      Normal")
+    print(" " * 31 + "No          Yes")
+    print(" " * 24 + "(1,2,6,7)          (8)")
+
+
+# Call the function
+print_decision_tree()
